6c86e606b3687d1ef0ee64360667ca26be2e7b1b
Dzmitry Kharevich <dzmitry.kharevich@gmail.com>
2022-08-25 11:48:46 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CoreDataProvider
+
+A description of this package.
---
+++ Tests/CoreDataProviderTests/CoreDataProviderTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CoreDataProvider
+
+final class CoreDataProviderTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CoreDataProvider().text, "Hello, World!")
+    }
+}
